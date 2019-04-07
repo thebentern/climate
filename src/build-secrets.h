4935b2b4e993173ad9178824af473747bdffb5e0
Benjamin Meadors <bmeadors@mainstream-tech.com>
2019-04-06 19:12:40 -0500

    Fix for CI

---
+++ src/build-secrets.h
@@ -0,0 +1,9 @@
+
+#define SSID "MyAp"
+#define WIFI_PASSWORD "MyAppPassword"
+
+#define MQTT_HOST "192.168.2.45"
+#define MQTT_TOPIC_CLIMATE "/climate"
+
+#define PWS_STATION_ID "1230sfasdf"
+#define PWS_PASSWORD "120asdfla"
